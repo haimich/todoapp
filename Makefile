79ad706ceb50560ec1c234039a3e6c83131d3969
Michael Müller <haimich@gmx.net>
2016-10-30 15:11:12 +0100

    added mysql docker container

--- Makefile
+++ Makefile
@@ -1,2 +1,5 @@
 start-server:
 	cd backend && composer run start
+
+start-db:
+	docker run --name mysql -p 3306:3306 -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=todos -e MYSQL_PASSWORD=todos -e MYSQL_DATABASE=todos -d mysql
--- backend/repositories/TodosRepository.php
+++ backend/repositories/TodosRepository.php
@@ -2,7 +2,7 @@
 
 class TodosRepository {
 
-  static $DB_HOST = 'localhost';
+  static $DB_HOST = '127.0.0.1';
   static $DB_USER = 'todos';
   static $DB_PW = 'todos';
   static $DB_DATABASE = 'todos';
@@ -10,13 +10,12 @@ class TodosRepository {
   private $db = null;
 
   function __construct() {
-    phpinfo();
-    // $this->db = mysqli_connect(
-    //   TodosRepository::$DB_HOST,
-    //   TodosRepository::$DB_USER,
-    //   TodosRepository::$DB_PW,
-    //   TodosRepository::$DB_DATABASE
-    // );
+    $this->db = mysqli_connect(
+      TodosRepository::$DB_HOST,
+      TodosRepository::$DB_USER,
+      TodosRepository::$DB_PW,
+      TodosRepository::$DB_DATABASE
+    );
   }
 
 }
